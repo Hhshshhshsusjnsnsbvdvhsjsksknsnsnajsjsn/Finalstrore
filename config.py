abd66f80b57ae6f2c84a905000ef655b88e5d9a5
Hhshshhshsusjnsnsbvdvhsjsksknsnsnajsjsn <koushalprasad731@gmail.com>
2024-12-23 20:13:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,30 +9,30 @@ from logging.handlers import RotatingFileHandler
 load_dotenv()
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", " ")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7648240074:AAGMLQVF4FP_IpWszcBhnqkubzUv2pX61M8")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", " "))
+APP_ID = int(os.environ.get("APP_ID", "29534418"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", " ")
+API_HASH = os.environ.get("API_HASH", "5f15dd792990ade40a43ae17413b422f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", " "))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002354086648"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5691486059"))
 
 #Port
-PORT = os.environ.get("PORT", "8037")
+PORT = os.environ.get("PORT", "8000")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", " ")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://fffworld57:Or97XLuv6P6GvtFZ@cluster0.u34tcer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesx")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", " "))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002433749621"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
@@ -57,7 +57,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 # Auto delete time in seconds.
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
+AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "10"))
 AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.")
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
 
